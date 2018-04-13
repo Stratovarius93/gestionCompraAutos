e9adb1345cb814cba8514501ca08a29b01014cec
MarcoEspinosaV <38263337+MarcoEspinosaV@users.noreply.github.com>
2018-04-13 03:07:01 -0500

    auto2

--- car.py
+++ car.py
@@ -23,70 +23,60 @@ class Buy:
         print ("\t4 - Nissan")
         print ("\t5 - Regresar al Menu principal")
 
+
+class Pay():
+    pass
+
+
 class Menu():
 
     while True:
         Buy().buying()
         options = input("Ingrese el numero de la opcion que desee")
 
-
         if options == 1:
             print "Lista de Autos disponibles Chevrolet"
             print Chevrolet.mark, " Modelo: ", Chevrolet.model, " Anio: ", Chevrolet.year, " Motor: ", Chevrolet.motor, " Kilometraje: ", Chevrolet.km, " Precio: ", Chevrolet.cost
+            pay = raw_input("desea comprar este auto?")
+            if pay == "si":
+                print "Compra exitosa"
+                break
+            elif pay == "no":
+                print "Regresando al menu de seleccion"
         elif options == 2:
             print "Lista de Autos disponibles Toyota"
             print Toyota.mark, " Modelo: ", Toyota.model, " Anio: ", Toyota.year, " Motor: ", Toyota.motor, " Kilometraje: ", Toyota.km, " Precio: ", Toyota.cost
+            print "desea comprar este auto?"
+            pay = raw_input()
+            if pay == "si":
+                print "Compra exitosa"
+                break
+            elif pay == "no":
+                print "Regresando al menu de seleccion"
         elif options == 3:
             print "Lista de Autos disponibles Honda"
             print Honda.mark, " Modelo: ", Honda.model, " Anio: ", Honda.year, " Motor: ", Honda.motor, " Kilometraje: ", Honda.km, " Precio: ", Honda.cost
+            print "desea comprar este auto?"
+            pay = raw_input()
+            if pay == "si":
+                print "Compra exitosa"
+                break
+            elif pay == "no":
+                print "Regresando al menu de seleccion"
         elif options == 4:
             print "Lista de Autos disponibles Nissan"
             print Nissan.mark, " Modelo: ", Nissan.model, " Anio: ", Nissan.year, " Motor: ", Nissan.motor, " Kilometraje: ", Nissan.km, " Precio: ", Nissan.cost
+            print "desea comprar este auto?"
+            pay = raw_input()
+            if pay == "si":
+                print "Compra exitosa"
+                break
+            elif pay == "no":
+                print "Regresando al menu de seleccion"
         elif options == 5:
             break
         else:
             print "Valor Incorrecto"
-
-class PayChevrolet():
-    print Chevrolet.mark, " Modelo: ", Chevrolet.model, " Anio: ", Chevrolet.year, " Motor: ", Chevrolet.motor, " Kilometraje: ", Chevrolet.km, " Precio: ", Chevrolet.cost
-    print "Tendra un precio total de ", Chevrolet.cost, " desea continuar con el pago?"
-    pay = input()
-    if pay == "si":
-        print "Compra exitosa"
-    elif pay == "no":
-        print "Regresando al menu de seleccion"
-
-
-class PayToyota():
-    print Toyota.mark, " Modelo: ", Toyota.model, " Anio: ", Toyota.year, " Motor: ", Toyota.motor, " Kilometraje: ", Toyota.km, " Precio: ", Toyota.cost
-    print "Tendra un precio total de ", Toyota.cost, " desea continuar con el pago?"
-    pay = input()
-    if pay == "si":
-        print "Compra exitosa"
-    elif pay == "no":
-        print "Regresando al menu de seleccion"
-
-
-class PayHonda():
-    print Honda.mark, " Modelo: ", Honda.model, " Anio: ", Honda.year, " Motor: ", Honda.motor, " Kilometraje: ", Honda.km, " Precio: ", Honda.cost
-    print "Tendra un precio total de ", Honda.cost, " desea continuar con el pago?"
-    pay = input()
-    if pay == "si":
-        print "Compra exitosa"
-    elif pay == "no":
-        print "Regresando al menu de seleccion"
-
-
-class PayNissan():
-    print Nissan.mark, " Modelo: ", Nissan.model, " Anio: ", Nissan.year, " Motor: ", Nissan.motor, " Kilometraje: ", Nissan.km, " Precio: ", Nissan.cost
-    print "Tendra un precio total de ", Nissan.cost, " desea continuar con el pago?"
-    pay = input()
-    if pay == "si":
-        print "Compra exitosa"
-    elif pay == "no":
-        print "Regresando al menu de seleccion"
-
-
 
 
 Menu()
