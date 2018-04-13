7107f749f2f49c017a7b9a1c4f7821f9a86be1c4
MarcoEspinosaV <38263337+MarcoEspinosaV@users.noreply.github.com>
2018-04-13 03:17:30 -0500

    auto3

--- car.py
+++ car.py
@@ -1,3 +1,5 @@
+import time
+
 class Car:
     def __init__(self, mark, year, model, motor, km, cost):
         self.mark = mark
@@ -32,47 +34,59 @@ class Menu():
 
     while True:
         Buy().buying()
-        options = input("Ingrese el numero de la opcion que desee")
+        options = input("Ingrese el numero de la opcion que desee\n")
 
         if options == 1:
             print "Lista de Autos disponibles Chevrolet"
+            time.sleep(2)
             print Chevrolet.mark, " Modelo: ", Chevrolet.model, " Anio: ", Chevrolet.year, " Motor: ", Chevrolet.motor, " Kilometraje: ", Chevrolet.km, " Precio: ", Chevrolet.cost
-            pay = raw_input("desea comprar este auto?")
+            time.sleep(4)
+            pay = raw_input("desea comprar este auto?\n")
             if pay == "si":
-                print "Compra exitosa"
-                break
+                print "Compra exitosa.....Regresando al menu de compra"
+                time.sleep(4)
             elif pay == "no":
                 print "Regresando al menu de seleccion"
+                time.sleep(4)
         elif options == 2:
             print "Lista de Autos disponibles Toyota"
+            time.sleep(2)
             print Toyota.mark, " Modelo: ", Toyota.model, " Anio: ", Toyota.year, " Motor: ", Toyota.motor, " Kilometraje: ", Toyota.km, " Precio: ", Toyota.cost
-            print "desea comprar este auto?"
+            time.sleep(4)
+            print "desea comprar este auto\n?"
             pay = raw_input()
             if pay == "si":
-                print "Compra exitosa"
-                break
+                print "Compra exitosa.....Regresando al menu de compra"
+                time.sleep(4)
             elif pay == "no":
                 print "Regresando al menu de seleccion"
+                time.sleep(4)
         elif options == 3:
             print "Lista de Autos disponibles Honda"
+            time.sleep(2)
             print Honda.mark, " Modelo: ", Honda.model, " Anio: ", Honda.year, " Motor: ", Honda.motor, " Kilometraje: ", Honda.km, " Precio: ", Honda.cost
-            print "desea comprar este auto?"
+            time.sleep(4)
+            print "desea comprar este auto\n?"
             pay = raw_input()
             if pay == "si":
-                print "Compra exitosa"
-                break
+                print "Compra exitosa.....Regresando al menu de compra"
+                time.sleep(4)
             elif pay == "no":
                 print "Regresando al menu de seleccion"
+                time.sleep(4)
         elif options == 4:
             print "Lista de Autos disponibles Nissan"
+            time.sleep(2)
             print Nissan.mark, " Modelo: ", Nissan.model, " Anio: ", Nissan.year, " Motor: ", Nissan.motor, " Kilometraje: ", Nissan.km, " Precio: ", Nissan.cost
-            print "desea comprar este auto?"
+            time.sleep(4)
+            print "desea comprar este auto\n?"
             pay = raw_input()
             if pay == "si":
-                print "Compra exitosa"
-                break
+                print "Compra exitosa.....Regresando al menu de compra"
+                time.sleep(4)
             elif pay == "no":
                 print "Regresando al menu de seleccion"
+                time.sleep(4)
         elif options == 5:
             break
         else:
