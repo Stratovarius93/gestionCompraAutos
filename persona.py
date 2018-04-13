edfd96e2066f05514e01a4a529052567f990ab6b
Stratovarius93 <www.warcry@gmail.com>
2018-04-13 02:09:36 -0500

    New update
    New update

--- persona.py
+++ persona.py
@@ -11,6 +11,7 @@ def menu():
     print "\t4- Eliminar"
     print "\t5- Regresar al Menú principal"
     print "\t6- Salir"
+    print
 
 def menuEditar():
     print
@@ -18,11 +19,11 @@ def menuEditar():
     print "Editar:"
     print "================================="
     print "\t1- Nombre"
-    print "\t2- Edad
-    "
+    print "\t2- Edad"
     print "\t3- Nacionalidad"
     print "\t4- Genero"
     print "\t5- Regresar al menú"
+    print
 
 #====CREAR====
 nombre=""
@@ -56,13 +57,22 @@ def calcularValorAuto():
 
 #======CRUD========
 def crear():
+    print "================================="
+    print "Crear:"
+    print "================================="
     print
     nombre=raw_input("Ingrese su nombre: ")
+    print
     identificacion=raw_input("Ingrese su identificación: ")
+    print
     edad=raw_input("Ingrese su edad: ")
+    print
     nacionalidad=raw_input("Ingrese su nacionalidad: ")
+    print
     genero=raw_input("Ingrese su género: ")
-    registro=raw_input("¿Desea agregar su registro?(SI/NO) >>")
+    print
+    registro=raw_input("¿Desea agregar su registro?(SI/NO) >> ")
+    print
     if registro=="Si" or registro=="SI" or registro=="si":
         persona=[nombre,identificacion,edad,nacionalidad,genero]
         arrayPersona.append(persona)
@@ -76,6 +86,10 @@ def crear():
         print
 def consultar():
     print
+    print "================================="
+    print "Consultar:"
+    print "================================="
+    print
     idConsulta=raw_input("Ingrese el número de identifiación: ")
     print (idConsulta in arrayIdentificacion)
     if (idConsulta in arrayIdentificacion)==True:
@@ -87,13 +101,20 @@ def consultar():
 
 def eliminar():
     print
+    print "================================="
+    print "Eliminar:"
+    print "================================="
+    print
     idEliminar=raw_input("Ingrese el número de identifiación: ")
     if (idEliminar in arrayIdentificacion)==True:
         posicion=arrayIdentificacion.index(idEliminar)
         print "Datos encontrados: "
-        print arrayPersona[posicion]
         print
-        respuesta=raw_input("¿Desea eliminarlo?(SI/NO)")
+        print "====================================="
+        print arrayPersona[posicion]
+        print "====================================="
+        print
+        respuesta=raw_input("¿Desea eliminarlo?(SI/NO) >>")
         if respuesta=="SI" or respuesta=="si" or respuesta=="Si":
             arrayPersona.pop(posicion)
             arrayIdentificacion.pop(posicion)
@@ -101,8 +122,10 @@ def eliminar():
             arrayEdad.pop(posicion)
             arrayNacionalidad.pop(posicion)
             arrayGenero.pop(posicion)
+            print
             print "Los datos se eliminaron correctamente."
         else:
+            print
             print "Cancelado"
     print
 
@@ -112,15 +135,23 @@ def editarNombre():
     if (idNombre in arrayIdentificacion)==True:
         posicion=arrayIdentificacion.index(idNombre)
         print "Datos encontrados: "
+        print
+        print "====================================="
         print arrayPersona[posicion]
+        print "====================================="
 
         nuevoNombre=raw_input("Ingrese el nuevo nombre: ");
         arrayNombre[posicion]=nuevoNombre
         nuevaPersona=[nuevoNombre,arrayIdentificacion[posicion],arrayEdad[posicion],arrayNacionalidad[posicion],arrayGenero[posicion]]
         arrayPersona[posicion]=nuevaPersona
 
+        print
         print arrayNombre[posicion]
+        print
+        print "====================================="
         print arrayPersona[posicion]
+        print "====================================="
+        print
         print "El nombre se cambió correctamente."
     else:
         print "No se pudo cambiar el nombre."
@@ -132,15 +163,23 @@ def editarEdad():
     if (idNombre in arrayIdentificacion)==True:
         posicion=arrayIdentificacion.index(idEdad)
         print "Datos encontrados: "
+        print
+        print "====================================="
         print arrayPersona[posicion]
+        print "====================================="
 
         nuevaEdad=raw_input("Ingrese la nueva edad: ");
         arrayEdad[posicion]=nuevaEdad
         nuevaPersona=[arrayNombre[posicion],arrayIdentificacion[posicion],nuevaEdad,arrayNacionalidad[posicion],arrayGenero[posicion]]
         arrayPersona[posicion]=nuevaPersona
 
+        print
         print arrayEdad[posicion]
+        print
+        print "====================================="
         print arrayPersona[posicion]
+        print "====================================="
+        print
         print "La edad se cambió correctamente."
     else:
         print "No se pudo cambiar la edad."
@@ -152,15 +191,22 @@ def editarNacionalidad():
     if (idNacionalidad in arrayIdentificacion)==True:
         posicion=arrayIdentificacion.index(idNacionalidad)
         print "Datos encontrados: "
+        print
+        print "====================================="
         print arrayPersona[posicion]
+        print "====================================="
 
         nuevaNacionalidad=raw_input("Ingrese la nueva nacionalidad: ");
         arrayNacionalidad[posicion]=nuevaNacionalidad
         nuevaPersona=[arrayNombre[posicion],arrayIdentificacion[posicion],arrayEdad[posicion],nuevaNacionalidad,arrayGenero[posicion]]
         arrayPersona[posicion]=nuevaPersona
-
+        print
         print arrayNacionalidad[posicion]
+        print
+        print "====================================="
         print arrayPersona[posicion]
+        print "====================================="
+        print
         print "La nacionalidad se cambió correctamente."
     else:
         print "No se pudo cambiar la nacionalidad."
@@ -172,15 +218,23 @@ def editarGenero():
     if (idGenero in arrayIdentificacion)==True:
         posicion=arrayIdentificacion.index(idGenero)
         print "Datos encontrados: "
+        print
+        print "====================================="
         print arrayPersona[posicion]
+        print "====================================="
 
         nuevoGenero=raw_input("Ingrese su nuevo género: ");
         arrayGenero[posicion]=nuevoGenero
         nuevaPersona=[arrayNombre[posicion],arrayIdentificacion[posicion],arrayEdad[posicion],arrayNacionalidad[posicion],nuevoGenero]
         arrayPersona[posicion]=nuevaPersona
 
+        print
         print arrayGenero[posicion]
+        print
+        print "====================================="
         print arrayPersona[posicion]
+        print "====================================="
+        print
         print "Su género se cambió correctamente."
     else:
         print "No se pudo cambiar el género."
